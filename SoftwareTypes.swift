6ccafc566c2f06bc749a2dacac16e51abcc801e4
Lawrence <lawrencehan94@gmail.com>
2017-10-18 23:50:08 -0700

    v3

--- SoftwareTypes.swift
+++ SoftwareTypes.swift
@@ -9,3 +9,30 @@ Engineering Software Companies
 
 Financial Software Companies 
 "SSNC,BR,FDS,FICO,ACIW,QTWO,EPAY,"
+
+Digital / Interactive Entertainment Software Companies
+"ATVI,EA,TTWO,ZNGA,"
+
+Infrastructure Software Companies 
+"VMW,RHT,CTXS,OTEX,NTNX,PRGS,ATEN,AVID"
+
+Business Intelligence Software Companies 
+"SPLK,DATA,NEWR,VRNS,HDP,MSTR"
+
+Systems Management Software Companies 
+"SYMC,NOW,CA,NTCT,CVLT"
+
+Internet Infrastructure Software Companies
+"AKAM,GDDY,EIGI,BSFT,LLNW,PFSW,RNWK"
+
+Cross-Industry Cloud Software Companies
+"ADBE,CRM,INTU,ADSK,WDAY,NOW,SQ,LOGM,ULTI,PAYC,PFPT,RNG,HUBS,ZEN,TWLO,TTD,BOX,PCTY,CSOD,BL,QLYS,CALD,FIVN,EGHT,WK,LPSN,APTI,BV,SREV,BCOV,SYNC,MRIN"
+
+Security Software Companies
+"SYMC,PANW,FTNT,PFPT,FEYE,QLYS,IMPV,CUDA,SCWX,RPD,VDSI,MOBL,ZIXI"
+
+Telecom / Mobile Software Companies
+Industry-Vertical Cloud Software Companies
+Industry-Vertical Software Companies
+Healthcare Software Companies 
+Education Software Companies 
