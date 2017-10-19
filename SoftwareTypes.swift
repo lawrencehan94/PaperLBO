09a0812232564f09020f2ed16690262bed112377
Lawrence <lawrencehan94@gmail.com>
2017-10-18 23:45:31 -0700

    v1

---
+++ SoftwareTypes.swift
@@ -0,0 +1,11 @@
+Large Cap, Diversified Companies
+"AAPL,GOOGL,MSFT,ORCL,IBM,CSCO,HPQ"
+
+Application Software Companies
+"ADBE,INTU,ADSK,NUAN,FICO,PEGA,MANH,CALD,TYPE,PRO,MODN,EGAN,"
+
+Engineering Software Companies 
+"ADSK,SNPS,CDNS,ANSS,PTC,AZPN,EXA,"
+
+Financial Software Companies 
+"SSNC,BR,FDS,FICO,ACIW,QTWO,EPAY,"
