0ae90bb37d9c1f3a265d0bf6c12aea75ad728019
Lawrence <lawrencehan94@gmail.com>
2017-10-23 16:12:59 -0700

    v1

---
+++ HardwareNames.swift
@@ -0,0 +1,136 @@
+case "ADI": return "Analog Devices"
+case "CRUS": return "Cirrus Logic"
+case "IDTI": return "IDT"
+case "PI": return "Impinj"
+case "IPHI": return "Inphi"
+case "MTSI": return "M/A-Com "
+case "MXIM": return "Maxim "
+case "MXL": return "Maxlinear"
+case "MPWR": return "Monolithic Power"
+case "POWI": return "Power Integrations"
+case "QRVO": return "Qorvo"
+case "SMTC": return "Semtech"
+case "SLAB": return "Silicon Labs"
+case "SWKS": return "Skyworks "
+case "TXN": return "Texas Instruments"
+
+
+case "ACIA": return "Acacia"
+case "AMBA": return "Ambarella"
+case "CAVM": return "Cavium"
+case "DGII": return "Digi Intl"
+case "MRVL": return "Marvell "
+case "MLNX": return "Mellanox "
+case "NPTN": return "Neophotonics"
+case "PXLW": return "Pixelworks"
+case "QCOM": return "Qualcomm "
+case "SIGM": return "Sigma Designs"
+case "SYNA": return "Synaptics"
+
+
+case "AMD": return "AMD"
+case "INTC": return "Intel"
+case "LSCC": return "Lattice "
+case "NVDA": return "Nvidia"
+case "QUIK": return "Quicklogic"
+case "XLNX": return "Xilinx"
+
+
+case "AOSL": return "Alpha & Omega "
+case "DIOD": return "Diodes"
+case "CREE": return "Cree"
+case "IXYS": return "Ixys"
+case "ON": return "On Semi"
+case "VSH": return "Vishay"
+
+
+case "AVGO": return "Broadcom"
+case "CY": return "Cypress "
+case "MCHP": return "Microchip"
+case "MSCC": return "Microsemi "
+case "NXPI": return "NXP"
+case "SIMO": return "Silicon Motion "
+case "VICR": return "Vicor"
+
+
+case "MU": return "Micron "
+
+
+case "KN": return "Knowles"
+case "ST": return "Sensata"
+case "SYNA": return "Synaptics"
+
+
+case "AAPL": return "Apple"
+case "BRCD": return "Brocade"
+case "CSCO": return "Cisco"
+case "CRY": return "Cray Supercomputers"
+case "HPQ": return "HP"
+case "IBM": return "IBM"
+case "JNPR": return "Juniper "
+case "MSFT": return "Microsoft"
+case "MSI": return "Motorola Sltns"
+case "NTAP": return "Netapp"
+case "ORCL": return "Oracle"
+
+
+case "CEVA": return "Ceva"
+case "DSPG": return "Dsp Group"
+case "IDCC": return "Interdigital"
+case "IMI": return "Intermolecular"
+case "PDFS": return "PDF Solutions"
+case "RMBS": return "Rambus"
+case "XPER": return "Xperi"
+
+
+case "AMAT": return "Applied Materials"
+case "ACLS": return "Axcelis"
+case "LRCX": return "Lam Research"
+case "VECO": return "Veeco Instruments"
+
+
+case "PLAB": return "Photronics"
+
+
+case "AEIS": return "Advanced Energy"
+case "BRKS": return "Brooks Automation"
+case "CGNX": return "Cognex"
+case "COHR": return "Coherent"
+case "MKSI": return "MKS Instruments"
+case "UCTT": return "Ultra Clean "
+
+
+case "KLAC": return "Kla-Tencor"
+case "NANO": return "Nanometrics "
+case "RTEC": return "Rudolph"
+case "VECO": return "Veeco"
+
+
+case "AME": return "Ametek"
+case "EXPO": return "Exponent"
+case "FORM": return "Formfactor"
+case "KEYS": return "Keysight"
+case "NATI": return "National Instruments"
+case "TER": return "Teradyne"
+case "VIAV": return "Viavi"
+case "XCRA": return "Xcerra"
+
+
+case "COHU": return "Cohu"
+case "ESIO": return "ESI"
+case "IVAC": return "Intevac"
+case "KLIC": return "Kulicke & Soffa"
+
+
+case "APD": return "Air Products"
+case "CCMP": return "Cabot Micro"
+case "EMKR": return "Emcore"
+case "ENTG": return "Entegris"
+case "KOPN": return "Kopin"
+
+
+case "AMKR": return "Amkor"
+
+
+case "CDNS": return "Cadence"
+case "SNPS": return "Synopsys"
