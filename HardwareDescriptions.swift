f72a4a9f08341fe3adc6f067854817a8892aa93f
Lawrence <lawrencehan94@gmail.com>
2017-10-23 16:25:05 -0700

    v3

--- HardwareDescriptions.swift
+++ HardwareDescriptions.swift
@@ -1,1 +1,125 @@
+case "IBM": return "International Business Machines Corporation provides information technology (IT) products and services worldwide."
+case "HPQ": return "HP Inc. provides products, technologies, software, solutions, and services to individual consumers, small- and medium-sized businesses, and large enterprises, including customers in the government, health, and education sectors worldwide."
+
+
+case "NTAP": return "NetApp, Inc. provides software, systems, and services to manage and store computer data worldwide."
+case "PSTG": return "Pure Storage, Inc. engages in building a data platform that enables businesses to enhance performance and reduce complexity and costs worldwide."
+
+
+case "VMW": return "VMware, Inc. provides virtualization and cloud infrastructure solutions in the United States and internationally."
+case "RHT": return "Red Hat, Inc. provides open source software solutions to develop and offer operating system, virtualization, management, middleware, cloud, mobile, and storage technologies to various enterprises worldwide."
+case "SYMC": return "Symantec Corporation, together with its subsidiaries, provides cybersecurity solutions worldwide."
+case "CA": return "CA, Inc. provides software and solutions that help organizations to plan, develop, manage, and secure applications and enterprise environments in the United States and internationally."
+case "CTXS": return "Citrix Systems, Inc. provides an integrated platform for secure app and data delivery, and network functionality as a cloud-based service worldwide."
+case "VRSN": return "VeriSign, Inc. provides domain name registry services and Internet security worldwide."
+case "CVLT": return "Commvault Systems, Inc. provides data and information management software applications and related services in the United States and internationally."
+
+
+case "SMCI": return "Super Micro Computer, Inc., together with its subsidiaries, develops and provides high performance server solutions based on modular and open architecture."
+
+
+case "CSCO": return "Cisco Systems, Inc. designs, manufactures, and sells Internet Protocol (IP) based networking and other products related to the communications and information technology industry worldwide."
+case "FFIV": return "F5 Networks, Inc. develops, markets, and sells application delivery networking products that optimize the security, performance, and availability of network applications, servers, and storage systems."
+case "BRCD": return "Brocade Communications Systems, Inc. provides storage area networking (SAN) and Internet protocol (IP) networking solutions for businesses and organizations worldwide."
+
+
+case "QTM": return "Quantum Corporation provides scale-out storage, archive, and data protection solutions for small businesses and multi-national enterprises in the Americas, Europe, and the Asia Pacific."
+
+
+case "WDC": return "Western Digital Corporation, together with its subsidiaries, develops, manufactures, and sells data storage devices and solutions worldwide."
+case "STX": return "Seagate Technology plc provides data storage technology and solutions in Singapore, the United States, the Netherlands, and internationally."
+
+
+case "CSCO": return "Cisco Systems, Inc. designs, manufactures, and sells Internet Protocol (IP) based networking and other products related to the communications and information technology industry worldwide."
+
+
+case "JNPR": return "Juniper Networks, Inc. designs, develops, and sells network products and services worldwide."
+case "BRCD": return "Brocade Communications Systems, Inc. provides storage area networking (SAN) and Internet protocol (IP) networking solutions for businesses and organizations worldwide."
+case "EXTR": return "Extreme Networks, Inc. provides software-driven networking solutions for enterprise customers worldwide."
+
+
+case "NTAP": return "NetApp, Inc. provides software, systems, and services to manage and store computer data worldwide."
+case "FFIV": return "F5 Networks, Inc. develops, markets, and sells application delivery networking products that optimize the security, performance, and availability of network applications, servers, and storage systems."
+case "IMPV": return "Imperva, Inc. engages in the development, market, sale, and support of cyber security solutions that protect business critical data and applications in the cloud or on premises worldwide."
+
+
+case "SYMC": return "Symantec Corporation, together with its subsidiaries, provides cybersecurity solutions worldwide."
+case "FTNT": return "Fortinet, Inc. provides cybersecurity solutions for enterprises, service providers, and government organizations worldwide."
+case "IMPV": return "Imperva, Inc. engages in the development, market, sale, and support of cyber security solutions that protect business critical data and applications in the cloud or on premises worldwide."
+case "DGII": return "Digi International Inc. provides Internet of Things networking hardware products and solutions."
+
+
+case "SONS": return "Sonus Networks, Inc. provides networked solutions worldwide."
+case "BSFT": return "BroadSoft, Inc. provides software and services that enable telecommunications service providers to deliver hosted cloud-based unified communications (UC) to their enterprise customers in North America, Europe, the Middle East, and Africa."
+
+
+case "INFN": return "Infinera Corporation provides optical transport networking equipment, software, and services worldwide."
+
+
+case "ADTN": return "ADTRAN, Inc. provides networking and communications equipment worldwide."
+case "CALX": return "Calix, Inc., together with its subsidiaries, develops, markets, and sells broadband communications access platforms, systems, and software for fiber- and copper-based network architectures that enable communications service providers (CSPs) to transform networks and connect to residential and business subscribers."
+case "DZSI": return "DASAN Zhone Solutions, Inc. provides network access solutions and communications equipment for service provider and enterprise networks worldwide."
+
+
+case "AAPL": return "Apple Inc. designs, manufactures, and markets mobile communication and media devices, personal computers, and portable digital music players to consumers, small and mid-sized businesses, and education, enterprise, and government customers worldwide."
+
+
+case "PLT": return "Plantronics, Inc. designs, manufactures, and markets lightweight communications headsets, telephone headset systems, other communication endpoints, and accessories for the business and consumer markets under the Plantronics brand worldwide."
+case "NTGR": return "NETGEAR, Inc. designs, develops, and markets innovative networking solutions and smart connected products for consumers, businesses, and service providers."
+
+
+case "ARRS": return "ARRIS International plc, together with its subsidiaries, provides media entertainment and data communications solutions in the United States and internationally."
+case "HLIT": return "Harmonic Inc. designs, manufactures, and sells video infrastructure products and system solutions worldwide."
+case "SEAC": return "SeaChange International, Inc. provides multiscreen video, advertising, and premium over the top (OTT) video products and services that facilitate the aggregation, licensing, management, and distribution of video and television advertising content to cable system operators, satellite operators, and telecommunications and media companies worldwide."
+
+
+case "MSI": return "Motorola Solutions, Inc. provides mission-critical communication infrastructure, devices, accessories, software, and services in North America, Latin America, the Asia Pacific, the Middle East, Europe, and Africa."
+case "GRMN": return "Garmin Ltd., together with its subsidiaries, designs, develops, manufactures, markets, and distributes a range of navigation, communication, and information devices worldwide."
+case "TRMB": return "Trimble Inc. provides technology solutions to enhance the work processes of office and mobile field professionals worldwide."
+case "HRS": return "Harris Corporation provides technology-based solutions that solve government and commercial customers’ mission-critical challenges in the United States and internationally."
+
+
+case "CVG": return "Convergys Corporation provides customer management services to communications and media, technology, financial services, retail, and healthcare industries in North America and internationally."
+case "NTCT": return "NetScout Systems, Inc. provides real-time operational intelligence and performance analytics for service assurance, and cybersecurity solutions in the United States, Europe, Asia, and internationally."
+case "CSGS": return "CSG Systems International, Inc. provides business support solutions primarily to the communications industry."
+
+
+case "A": return "Agilent Technologies, Inc. provides application focused solutions to the life sciences, diagnostics, and applied chemical markets worldwide."
+case "NATI": return "National Instruments Corporation designs, manufactures, and sells systems to engineers and scientists worldwide."
+
+
+case "GLW": return "Corning Incorporated manufactures and sells specialty glasses, ceramics, and related materials in North America, the Asia Pacific, Europe, and internationally."
+case "FNSR": return "Finisar Corporation provides optical subsystems and components for data communication and telecommunication applications in the United States, China, Malaysia, and internationally."
+case "COHR": return "Coherent, Inc. provides lasers and laser-based technology in a range of scientific, commercial, and industrial applications worldwide."
+case "OCLR": return "Oclaro, Inc. designs, manufactures, and markets optical components, modules, and subsystems for the long-haul, metro, and data center markets worldwide."
+
+
+case "QCOM": return "QUALCOMM Incorporated develops, designs, manufactures, and markets digital communications products and services in China, South Korea, Taiwan, the United States, and internationally."
+case "INTC": return "Intel Corporation designs, manufactures, and sells computer, networking, and communications platforms worldwide."
+case "TXN": return "Texas Instruments Incorporated designs, manufactures, and sells semiconductors to electronics designers and manufacturers worldwide."
+case "CAVM": return "Cavium, Inc. designs, develops, and markets semiconductor processors for intelligent and secure networks in the United States and internationally."
+
+
+case "MSFT": return "Microsoft Corporation develops, licenses, and supports software products, services, and devices worldwide."
+case "VMW": return "VMware, Inc. provides virtualization and cloud infrastructure solutions in the United States and internationally."
+case "CRM": return "Salesforce.com, inc. develops enterprise cloud computing solutions with a focus on customer relationship management."
+case "CTXS": return "Citrix Systems, Inc. provides an integrated platform for secure app and data delivery, and network functionality as a cloud-based service worldwide."
+case "RHT": return "Red Hat, Inc. provides open source software solutions to develop and offer operating system, virtualization, management, middleware, cloud, mobile, and storage technologies to various enterprises worldwide."
+
+
+case "IBM": return "International Business Machines Corporation provides information technology (IT) products and services worldwide."
+case "ORCL": return "Oracle Corporation develops, manufactures, markets, sells, hosts, and supports application, platform, and infrastructure technologies for information technology (IT) environments worldwide."
+case "HPQ": return "HP Inc. provides products, technologies, software, solutions, and services to individual consumers, small- and medium-sized businesses, and large enterprises, including customers in the government, health, and education sectors worldwide."
+
+
+case "VZ": return "Verizon Communications Inc., through its subsidiaries, provides communications, information, and entertainment products and services to consumers, businesses, and governmental agencies worldwide."
+case "TWX": return "Time Warner Inc. operates as a media and entertainment company in the United States and internationally."
+case "S": return "Sprint Corporation, through its subsidiaries, provides various wireless and wireline communications products and services to consumers, businesses, government subscribers, and resellers in the United States, Puerto Rico, and the U.S. Virgin Islands."
+case "CTL": return "CenturyLink, Inc. provides various communications services to residential, business, wholesale, and governmental customers in the United States."
+case "LVLT": return "Level 3 Communications, Inc., together with its subsidiaries, operates as a facilities-based provider of a range of integrated communications services."
+case "FTR": return "Frontier Communications Corporation provides communications services to residential, business, and wholesale customers in the United States."
+
+
+case "SSNI": return "Silver Spring Networks, Inc. operates as a networking platform and solutions provider for smart energy networks."
+case "CTRL": return "Control4 Corporation provides smart home and business solutions for the connected home or business in the United States, Canada, and internationally."
 
