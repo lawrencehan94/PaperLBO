8f8726743808389737b24a381fdd8dbe872d044a
Lawrence <lawrencehan94@gmail.com>
2017-10-23 16:24:33 -0700

    v3

--- HardwareNames.swift
+++ HardwareNames.swift
@@ -1,1 +1,124 @@
+case "IBM": return "IBM"
+case "HPQ": return "HP"
 
+
+case "NTAP": return "NetApp"
+case "PSTG": return "Pure Storage"
+
+
+case "VMW": return "Vmware"
+case "RHT": return "Red Hat"
+case "SYMC": return "Symantec"
+case "CA": return "CA"
+case "CTXS": return "Citrix"
+case "VRSN": return "Verisign"
+case "CVLT": return "Commvault Systems"
+
+
+case "SMCI": return "Super Micro Computer"
+
+
+case "CSCO": return "Cisco"
+case "FFIV": return "F5 Networks"
+case "BRCD": return "Brocade Communications Systems"
+
+
+case "QTM": return "Quantum"
+
+
+case "WDC": return "Western Digital"
+case "STX": return "Seagate Digital Technology"
+
+
+case "CSCO": return "Cisco"
+
+
+case "JNPR": return "Juniper Networks"
+case "BRCD": return "Brocade"
+case "EXTR": return "Extreme Networks"
+
+
+case "NTAP": return "Netapp"
+case "FFIV": return "F5 Networks"
+case "IMPV": return "Imperva"
+
+
+case "SYMC": return "Symantec Corpo"
+case "FTNT": return "Fortinet"
+case "IMPV": return "Imperva"
+case "DGII": return "Digi International"
+
+
+case "SONS": return "Sonus Networks"
+case "BSFT": return "Broadsoft"
+
+
+case "INFN": return "Infinera"
+
+
+case "ADTN": return "Adtran"
+case "CALX": return "Calix"
+case "DZSI": return "Dasan Zhone Solutions"
+
+
+case "AAPL": return "Apple"
+
+
+case "PLT": return "Plantronics"
+case "NTGR": return "Netgear"
+
+
+case "ARRS": return "Arris International"
+case "HLIT": return "Harmonic"
+case "SEAC": return "Seachange International"
+
+
+case "MSI": return "Motorla"
+case "GRMN": return "Garmin"
+case "TRMB": return "Trimble"
+case "HRS": return "Harris Corporation"
+
+
+case "CVG": return "Convergys"
+case "NTCT": return "Netscout Systems"
+case "CSGS": return "CSG Systems International"
+
+
+case "A": return "Agilent Technologies"
+case "NATI": return "National Instruments"
+
+
+case "GLW": return "Corning"
+case "FNSR": return "Finisar "
+case "COHR": return "Coherent"
+case "OCLR": return "Oclaro"
+
+
+case "QCOM": return "Qualcomm"
+case "INTC": return "Intel"
+case "TXN": return "Texas Instruments"
+case "CAVM": return "Cavium"
+
+
+case "MSFT": return "Microsoft"
+case "VMW": return "Vmware"
+case "CRM": return "Salesforce.Com"
+case "CTXS": return "Citrix Systems"
+case "RHT": return "Red Hat"
+
+
+case "IBM": return "IBM"
+case "ORCL": return "Oracle"
+case "HPQ": return "HP"
+
+
+case "VZ": return "Verizon"
+case "TWX": return "Time Warner"
+case "S": return "Sprint"
+case "CTL": return "Centurylink"
+case "LVLT": return "Level 3 Communications"
+case "FTR": return "Frontier"
+
+
+case "SSNI": return "Silver Spring Networks"
+case "CTRL": return "Control4 Corporation"
