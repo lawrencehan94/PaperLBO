3fd8adbdef9887ef032fffce79aacf4332db8bea
Lawrence <lawrencehan94@gmail.com>
2017-10-23 16:32:34 -0700

    v1

---
+++ SemisLocations.swift
@@ -0,0 +1,129 @@
+case "ADI": return "Norwood, MA"
+case "CRUS": return "Austin, TX"
+case "IDTI": return "San Jose, CA"
+case "PI": return "Seattle, WA"
+case "IPHI": return "Santa Clara, CA"
+case "MTSI": return "Lowell, MA"
+case "MXIM": return "San Jose, CA"
+case "MXL": return "Carlsbad, CA"
+case "MPWR": return "San Jose, CA"
+case "POWI": return "San Jose, CA"
+case "QRVO": return "Greensboro, NC"
+case "SMTC": return "Camarillo, CA"
+case "SLAB": return "Austin, TX"
+case "SWKS": return "Woburn, MA"
+case "TXN": return "Dallas, TX"
+
+
+case "ACIA": return "Maynard, MA"
+case "AMBA": return "Santa Clara, CA"
+case "CAVM": return "San Jose, CA"
+case "DGII": return "Minnetonka, MN"
+case "MRVL": return "Hamilton, N.A."
+case "MLNX": return "Yokneam, N.A."
+case "NPTN": return "San Jose, CA"
+case "PXLW": return "San Jose, CA"
+case "QCOM": return "San Diego, CA"
+case "SIGM": return "Fremont, CA"
+case "SYNA": return "San Jose, CA"
+
+
+case "AMD": return "Sunnyvale, CA"
+case "INTC": return "Santa Clara, CA"
+case "LSCC": return "Portland, OR"
+case "NVDA": return "Santa Clara, CA"
+case "QUIK": return "Sunnyvale, CA"
+case "XLNX": return "San Jose, CA"
+
+
+case "AOSL": return "Sunnyvale, CA"
+case "DIOD": return "Plano, TX"
+case "CREE": return "Durham, NC"
+case "ON": return "Phoenix, AZ"
+case "VSH": return "Malvern, PA"
+
+
+case "AVGO": return "Singapore, N.A."
+case "CY": return "San Jose, CA"
+case "MCHP": return "Chandler, AZ"
+case "MSCC": return "Aliso Viejo, CA"
+
+
+case "MU": return "Boise, ID"
+
+
+case "KN": return "Itasca, IL"
+case "ST": return "Hengelo, N.A."
+case "SYNA": return "San Jose, CA"
+
+
+case "AAPL": return "Cupertino, CA"
+case "BRCD": return "San Jose, CA"
+case "CSCO": return "San Jose, CA"
+case "CRY": return "Kennesaw, GA"
+case "HPQ": return "Palo Alto, CA"
+case "IBM": return "Armonk, NY"
+case "JNPR": return "Sunnyvale, CA"
+case "MSFT": return "Redmond, WA"
+case "MSI": return "Chicago, IL"
+case "NTAP": return "Sunnyvale, CA"
+case "ORCL": return "Redwood City, CA"
+
+
+case "CEVA": return "Mountain View, CA"
+case "DSPG": return "Los Altos, CA"
+case "IDCC": return "Wilmington, DE"
+case "PDFS": return "San Jose, CA"
+case "RMBS": return "Sunnyvale, CA"
+
+
+case "AMAT": return "Santa Clara, CA"
+case "ACLS": return "Beverly, MA"
+case "LRCX": return "Fremont, CA"
+case "VECO": return "Plainview, NY"
+
+
+case "PLAB": return "Brookfield, CT"
+
+
+case "AEIS": return "Fort Collins, CO"
+case "BRKS": return "Chelmsford, MA"
+case "CGNX": return "Natick, MA"
+case "COHR": return "Santa Clara, CA"
+case "MKSI": return "Andover, MA"
+case "UCTT": return "Hayward, CA"
+
+
+case "KLAC": return "Milpitas, CA"
+case "NANO": return "Milpitas, CA"
+case "RTEC": return "Wilmington, MA"
+case "VECO": return "Plainview, NY"
+
+
+case "AME": return "Berwyn, PA"
+case "EXPO": return "Menlo Park, CA"
+case "FORM": return "Livermore, CA"
+case "KEYS": return "Santa Rosa, CA"
+case "NATI": return "Austin, TX"
+case "TER": return "North Reading, MA"
+case "VIAV": return "Milpitas, CA"
+case "XCRA": return "Norwood, MA"
+
+
+case "COHU": return "Poway, CA"
+case "ESIO": return "Portland, OR"
+case "IVAC": return "Santa Clara, CA"
+case "KLIC": return "Singapore, N.A."
+
+
+case "APD": return "Allentown, PA"
+case "EMKR": return "Alhambra, CA"
+case "ENTG": return "Billerica, MA"
+case "KOPN": return "Westborough, MA"
+
+
+case "AMKR": return "Tempe, AZ"
+
+
+case "CDNS": return "San Jose, CA"
+case "SNPS": return "Mountain View, CA"
