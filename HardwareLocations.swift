6b06957ba067aa38669265c539a4f18e87d3c7a8
Lawrence <lawrencehan94@gmail.com>
2017-10-23 16:25:34 -0700

    v1

---
+++ HardwareLocations.swift
@@ -0,0 +1,124 @@
+case "IBM": return "Armonk, NY"
+case "HPQ": return "Palo Alto, CA"
+
+
+case "NTAP": return "Sunnyvale, CA"
+case "PSTG": return "Mountain View, CA"
+
+
+case "VMW": return "Palo Alto, CA"
+case "RHT": return "Raleigh, NC"
+case "SYMC": return "Mountain View, CA"
+case "CA": return "New York, NY"
+case "CTXS": return "Fort Lauderdale, FL"
+case "VRSN": return "Reston, VA"
+case "CVLT": return "Tinton Falls, NJ"
+
+
+case "SMCI": return "San Jose, CA"
+
+
+case "CSCO": return "San Jose, CA"
+case "FFIV": return "Seattle, WA"
+case "BRCD": return "San Jose, CA"
+
+
+case "QTM": return "San Jose, CA"
+
+
+case "WDC": return "San Jose, CA"
+case "STX": return "Cupertino, CA"
+
+
+case "CSCO": return "San Jose, CA"
+
+
+case "JNPR": return "Sunnyvale, CA"
+case "BRCD": return "San Jose, CA"
+case "EXTR": return "San Jose, CA"
+
+
+case "NTAP": return "Sunnyvale, CA"
+case "FFIV": return "Seattle, WA"
+case "IMPV": return "Redwood Shores, CA"
+
+
+case "SYMC": return "Mountain View, CA"
+case "FTNT": return "Sunnyvale, CA"
+case "IMPV": return "Redwood Shores, CA"
+case "DGII": return "Minnetonka, MN"
+
+
+case "SONS": return "Westford, MA"
+case "BSFT": return "Gaithersburg, MD"
+
+
+case "INFN": return "Sunnyvale, CA"
+
+
+case "ADTN": return "Huntsville, AL"
+case "CALX": return "Petaluma, CA"
+case "DZSI": return "Oakland, CA"
+
+
+case "AAPL": return "Cupertino, CA"
+
+
+case "PLT": return "Santa Cruz, CA"
+case "NTGR": return "San Jose, CA"
+
+
+case "ARRS": return "Suwanee, GA"
+case "HLIT": return "San Jose, CA"
+case "SEAC": return "Acton, MA"
+
+
+case "MSI": return "Chicago, IL"
+case "GRMN": return "Schaffhausen, N.A."
+case "TRMB": return "Sunnyvale, CA"
+case "HRS": return "Melbourne, FL"
+
+
+case "CVG": return "Cincinnati, OH"
+case "NTCT": return "Westford, MA"
+case "CSGS": return "Englewood, CO"
+
+
+case "A": return "Santa Clara, CA"
+case "NATI": return "Austin, TX"
+
+
+case "GLW": return "Corning, NY"
+case "FNSR": return "Sunnyvale, CA"
+case "COHR": return "Santa Clara, CA"
+case "OCLR": return "San Jose, CA"
+
+
+case "QCOM": return "San Diego, CA"
+case "INTC": return "Santa Clara, CA"
+case "TXN": return "Dallas, TX"
+case "CAVM": return "San Jose, CA"
+
+
+case "MSFT": return "Redmond, WA"
+case "VMW": return "Palo Alto, CA"
+case "CRM": return "San Francisco, CA"
+case "CTXS": return "Fort Lauderdale, FL"
+case "RHT": return "Raleigh, NC"
+
+
+case "IBM": return "Armonk, NY"
+case "ORCL": return "Redwood City, CA"
+case "HPQ": return "Palo Alto, CA"
+
+
+case "VZ": return "New York, NY"
+case "TWX": return "New York, NY"
+case "S": return "Overland Park, KS"
+case "CTL": return "Monroe, LA"
+case "LVLT": return "Broomfield, CO"
+case "FTR": return "Norwalk, CT"
+
+
+case "SSNI": return "San Jose, CA"
+case "CTRL": return "Salt Lake City, UT"
