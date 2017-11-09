c4bdebeaa884e778d98bd61545621caa1b5625c7
Lawrence <lawrencehan94@gmail.com>
2017-11-08 17:05:48 -0800

    v1

--- Deal.swift
+++ Deal.swift
@@ -16,6 +16,7 @@ class Deal {
 	let thirtyDay: Double
 	let unaffectedMonth: Int
 	let unaffectedYear: Int
+	let description: String
 	
 	// Calculated Inputs
 	let ntmRevGrowth: Double
@@ -24,7 +25,7 @@ class Deal {
 	let ltmNIMargin: Double
 	let ntmNIMargin: Double 
 	
-	init(_ announceMonthInput: Int, _ announceYearInput: Int, _ targetInput: String, _ acquirorInput: String, _ equityValueInput: Double, _ enterpriseValueInput: Double, _ ltmEVRevInput: Double, _ ntmEVRevInput: Double, _ ltmEVEBITDAInput: Double, _ ntmEVEBITDAInput: Double, _ ltmPEInput: Double, _ ntmPEInput: Double, _ oneDayInput: Double, _ thirtyDayInput: Double, _ unaffectedMonthInput: Int, _ unaffectedYearInput: Int) {
+	init(_ announceMonthInput: Int, _ announceYearInput: Int, _ targetInput: String, _ acquirorInput: String, _ equityValueInput: Double, _ enterpriseValueInput: Double, _ ltmEVRevInput: Double, _ ntmEVRevInput: Double, _ ltmEVEBITDAInput: Double, _ ntmEVEBITDAInput: Double, _ ltmPEInput: Double, _ ntmPEInput: Double, _ oneDayInput: Double, _ thirtyDayInput: Double, _ unaffectedMonthInput: Int, _ unaffectedYearInput: Int, _ descriptionInput: String) {
 	announceMonth = announceMonthInput
 	announceYear = announceYearInput  
 	target = targetInput
@@ -41,6 +42,7 @@ class Deal {
 	thirtyDay = thirtyDayInput
 	unaffectedMonth = unaffectedMonthInput
 	unaffectedYear = unaffectedYearInput
+	description = descriptionInput
 		
 	ntmRevGrowth = (ltmEVRev / ntmEVRev) - 1
 	ltmEBITDAMargin = ltmEVRev / ltmEVEBITDA
@@ -57,10 +59,11 @@ class Deal {
 var deals = [Deal]()
 
 func addSoftwareDeals() {
-	deals.append(Deal(10, 2017, "Gigamon", "Elliott Management", 1484.6, 1225.7, 4, 3.6, 31.6, 18, 0, 42.2, 0.215, 0.098, 4, 2017))
+	deals.append(Deal(10, 2017, "Gigamon", "Elliott Management", 1484.6, 1225.7, 4, 3.6, 31.6, 18, 0, 42.2, 0.215, 0.098, 4, 2017, "A software company for special people"))
 }
 
 addSoftwareDeals()
 print(deals[0].ntmEBITDAMargin)
+print(deals[0].description)
 
 	
